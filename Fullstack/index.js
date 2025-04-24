9f145b15640444520c2e686d32f00bea451d2374
Varun <varunsingh7004@gmail.com>
2025-04-25 04:14:47 +0530

    good

--- Fullstack/index.js
+++ Fullstack/index.js
@@ -1,11 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port = 3000; 
+const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Vader!n");
+  res.send("Vader!");
 });
+
+app.get("/hitesh", (req, res) => {
+  res.send("HitesSirOp!");
+});
+
+console.log(process.env.PORT);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
