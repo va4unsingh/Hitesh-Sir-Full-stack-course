9dd72f7f985b039a28d25279e03855fff44395b3
Varun <varunsingh7004@gmail.com>
2025-04-26 04:10:11 +0530

    routes

---
+++ Fullstack/Model/User.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const usersSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+    },
+    resetPasswordToken: {
+      type: String,
+    },
+    resetPasswordExpires: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", usersSchema);
+
+export default User;
