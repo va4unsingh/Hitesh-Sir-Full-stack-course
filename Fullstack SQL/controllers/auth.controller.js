34e6a297efea9412facbb38da05d76262de00263
Varun <varunsingh7004@gmail.com>
2025-05-11 02:34:45 +0530

    done for night

--- Fullstack SQL/controllers/auth.controller.js
+++ Fullstack SQL/controllers/auth.controller.js
@@ -1,6 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 const registerUser = async (req, res) => {
   console.log("User Registerd");
+  await prisma.user.findUnique({
+    where: { email },
+  });
 };
-
 
 export { registerUser };
