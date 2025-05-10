c03c04d1d8ea6e1385f457ad049818320049c4ae
Varun <varunsingh7004@gmail.com>
2025-05-11 00:35:34 +0530

    basic server

--- Fullstack SQL/index.js
+++ Fullstack SQL/index.js
@@ -1,1 +1,24 @@
 import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config();
+const port = process.env.PORT || 4000;
+const app = express();
+
+app.use(cookieParser);
+app.use(
+  cors({
+    origin: "process.env.BASE_URL",
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`Backend is listening at port: ${[port]}`);
+});
