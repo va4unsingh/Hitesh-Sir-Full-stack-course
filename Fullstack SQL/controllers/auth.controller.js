2fe8a4b870015dcb06123e29c1fba8144d1ae03a
Varun Singh <varunsingh7004@gmail.com>
2025-06-26 17:41:06 +0530

    start

--- Fullstack SQL/controllers/auth.controller.js
+++ Fullstack SQL/controllers/auth.controller.js
@@ -1,12 +1,116 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
 const registerUser = async (req, res) => {
-  console.log("User Registerd");
-  await prisma.user.findUnique({
-    where: { email },
-  });
+  const { name, email, password, phone } = req.body;
+  if (!name || !email || !password || !phone) {
+    console.log("Data is missing");
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required",
+    });
+  }
+  try {
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (!existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User Already Exist",
+      });
+    }
+    // hash the pass
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const verificationToken = crypto.randomBytes(32).toString("hex");
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        phone,
+        password: hashedPassword,
+        verificationToken,
+      },
+    });
+
+    // send mail - TODO
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error,
+      message: "Registration failed",
+    });
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    console.log("Data is missing");
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required",
+    });
+  }
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const isMatch = bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user.id, role: user.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "24h",
+      }
+    );
+
+    const cookieOptions = {
+      httpOnly: true,
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    };
+    res.cookie("token", token, cookieOptions);
+
+    return res.status(201).json({
+      success: true,
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+      message: "Login Done",
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: "Login failed",
+    });
+  }
 };
 
 export { registerUser };
